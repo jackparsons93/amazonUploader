c2b6ecf79dea1a545119ac487ce42fc1688af0e8
chris wilson <jackparsons93@gmail.com>
2023-01-25 01:32:29 -0600

    Add project files.

---
+++ amazonUploader/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace amazonUploader
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string bucketName = "jackparsons93";
+        private const string keyName = "test.png";
+        private const string filePath = "c:/users/jackparsons210/desktop/giphy.gif";
+        // Specify your bucket region (an example region is shown).
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.USEast2;
+        private static IAmazonS3 s3Client;
+
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                textBox1.Text = filename;
+            }
+            
+            //UploadFileAsync();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            s3Client = new AmazonS3Client(bucketRegion);
+            UploadFileAsync(textBox1.Text);
+
+        }
+        private static async Task UploadFileAsync(string file)
+        {
+            try
+            {
+                var fileTransferUtility =
+                    new TransferUtility(s3Client);
+
+                // Option 1. Upload a file. The file name is used as the object key name.
+                await fileTransferUtility.UploadAsync(file, bucketName);
+                Console.WriteLine("Upload 1 completed");
+            }
+
+
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine("Error encountered on server. Message:'{0}' when writing an object", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown encountered on server. Message:'{0}' when writing an object", e.Message);
+            }
+
+        }
+    }
+}
